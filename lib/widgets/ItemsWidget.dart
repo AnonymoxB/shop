41a1438ce9f2b57b359fe65f9afd5634c0f8e5b2
AnonymoxB <Anonymoxb12@gmail.com>
2024-04-28 23:37:47 +0700

    y

--- lib/widgets/ItemsWidget.dart
+++ lib/widgets/ItemsWidget.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class ItemsWidget extends StatelessWidget {
@@ -7,97 +5,102 @@ class ItemsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
       childAspectRatio: 0.68,
-      // physics: NeverScrollableScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
       crossAxisCount: 2,
       shrinkWrap: true,
       children: [
-        Container(
-          padding: EdgeInsets.only(
-            left: 15,
-            right: 15,
-            top: 10,
-          ),
-          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 34, 104, 235),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Text(
-                    "-50%",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Icon(
-                  Icons.favorite_border,
-                  color: Colors.red,
-                ),
-              ],
+        for (int i = 1; i < 10; i++)
+          Container(
+            padding: EdgeInsets.only(
+              left: 15,
+              right: 15,
+              top: 10,
             ),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: Image.asset(
-                  "images/1.png",
-                  height: 120,
-                  width: 120,
-                ),
-              ),
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
             ),
-            Container(
-              padding: EdgeInsets.only(bottom: 8),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Product Title",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Color.fromARGB(255, 34, 104, 235),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Write Description of product",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Color.fromARGB(255, 34, 104, 235),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Row(
+            child: Column(children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Rp.350.000",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
                       color: Color.fromARGB(255, 34, 104, 235),
+                      borderRadius: BorderRadius.circular(20),
                     ),
+                    child: Text(
+                      "-50%",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
                   ),
                 ],
               ),
-            ),
-          ]),
-        ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Image.asset(
+                    "images/1.png",
+                    height: 120,
+                    width: 120,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(bottom: 8),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Product Title",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 34, 104, 235),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Write Description of product",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromARGB(255, 34, 104, 235),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Rp.350.000",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 34, 104, 235),
+                      ),
+                    ),
+                    Icon(
+                      Icons.shopping_cart_checkout,
+                      color: Color.fromARGB(255, 34, 104, 235),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
       ],
     );
   }
